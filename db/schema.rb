abe8e1e2608de2ab0d08e6eaf1c04e5095c1c6d5
Matt Davies <tonmatt@gmail.com>
2016-02-10 13:00:15 +0000

    adding ansible vagrant

--- db/schema.rb
+++ db/schema.rb
@@ -14,43 +14,43 @@
 ActiveRecord::Schema.define(version: 20150826083958) do
 
   create_table "access_grants", force: :cascade do |t|
-    t.string   "code"
-    t.string   "access_token"
-    t.string   "refresh_token"
+    t.string   "code",                    limit: 255
+    t.string   "access_token",            limit: 255
+    t.string   "refresh_token",           limit: 255
     t.datetime "access_token_expires_at"
-    t.integer  "user_id"
-    t.integer  "client_id"
-    t.string   "state"
+    t.integer  "user_id",                 limit: 4
+    t.integer  "client_id",               limit: 4
+    t.string   "state",                   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "clients", force: :cascade do |t|
-    t.string   "name"
-    t.string   "app_id"
-    t.string   "app_secret"
+    t.string   "name",       limit: 255
+    t.string   "app_id",     limit: 255
+    t.string   "app_secret", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
